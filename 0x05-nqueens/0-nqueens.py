4f7e90c48a08fd5d010a57c20590f624d81bdd65
Caleb Nyachwaya <113683930+CalebNyachwaya@users.noreply.github.com>
2024-01-11 23:55:12 +0300

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,70 +1,39 @@
 #!/usr/bin/python3
-"""
-Module for 0x0C. N Queens.
-Holberton School
-Specializations - Interview Preparation ― Algorithms
-"""
-from sys import argv, exit
+""" program that solves the N queens problem """
+import sys
 
 
-def solveNQueens(n):
-    """Program that solves the N queens problem"""
-    res = []
-    queens = [-1] * n
-    # queens is a one-dimension array, like [1, 3, 0, 2] means
-    # index represents row no and value represents col no
-
-    def dfs(index):
-        """Recursively resolves the N queens problem"""
-        if index == len(queens):  # n queens have been placed correctly
-            res.append(queens[:])
-            return  # backtracking
-        for i in range(len(queens)):
-            queens[index] = i
-            if valid(index):  # pruning
-                dfs(index + 1)
-
-    # check whether nth queens can be placed
-    def valid(n):
-        """Method that checks if a position in the board is valid"""
-        for i in range(n):
-            if abs(queens[i] - queens[n]) == n - i:  # same diagonal
-                return False
-            if queens[i] == queens[n]:  # same column
-                return False
-        return True
-
-    # given queens = [1,3,0,2] this function returns
-    # [[0, 1], [1, 3], [2, 0], [3, 2]]
-
-    def make_all_boards(res):
-        """Method that builts the List that be returned"""
-        actual_boards = []
-        for queens in res:
-            board = []
-            for row, col in enumerate(queens):
-                board.append([row, col])
-            actual_boards.append(board)
-        return actual_boards
-
-    dfs(0)
-    return make_all_boards(res)
+def isSafe(Board, line, i):
+    """ checks if we can insert queen at column i in that line in Board"""
+    for x in range(line):
+        if (Board[x] == i or
+                Board[x] + line - x == i or
+                Board[x] + x - line == i):
+            return False
+    return True
 
 
-if __name__ == "__main__":
-    if len(argv) < 2:
-        print('Usage: nqueens N')
-        exit(1)
-    try:
-        n = int(argv[1])
-    except ValueError:
-        print('N must be a number')
-        exit(1)
+def Fill_line(Board, line):
+    """ fills each line of the board with the correct index """
+    for i in range(len(Board)):
+        if isSafe(Board, line, i):
+            Board[line] = i
+            if line < len(Board) - 1:
+                Fill_line(Board, line + 1)
+            else:
+                print([[i, Board[i]] for i in range(len(Board))])
 
-    if n < 4:
-        print('N must be at least 4')
-        exit(1)
-    else:
-        result = solveNQueens(n)
-        for row in result:
-            print(row)
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    sys.exit(1)
+try:
+    n = int(sys.argv[1])
+except:
+    print("N must be a number")
+    sys.exit(1)
+if n < 4:
+    print("N must be at least 4")
+    sys.exit(1)
+
+Board = [-1 for i in range(n)]
+Fill_line(Board, 0)
